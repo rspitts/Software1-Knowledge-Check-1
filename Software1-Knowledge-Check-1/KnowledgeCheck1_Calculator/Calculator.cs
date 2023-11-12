12a29433b970a5b8f3a94f253513a69d83122be9
Robert Pitts <spencerpitts94@gmail.com>
2023-11-12 18:09:03 -0500

    Add files via upload

---
+++ Software1-Knowledge-Check-1/KnowledgeCheck1_Calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck1_Calculator
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        public int Multiply(int first, int second)
+        {
+			return first * second;
+		}
+
+        public double Divide(double first, double second)
+        {
+            return first / second;
+        }
+
+		public static void ErrorMessage()
+		{
+			Console.WriteLine("One or more of the numbers is not an int");
+		}
+	}
+}
